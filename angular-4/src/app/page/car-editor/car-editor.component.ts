f5b206cdb3d17e1f51ec570c6c51e00f0e4bdcb5
keszigabesz <91908624+keszigabesz@users.noreply.github.com>
2022-02-28 16:38:12 +0100

    4 majdnem kész

---
+++ angular-4/src/app/page/car-editor/car-editor.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Observable, switchMap } from 'rxjs';
+import { Car } from 'src/app/model/car';
+import { CarService } from 'src/app/service/car.service';
+
+@Component({
+  selector: 'app-car-editor',
+  templateUrl: './car-editor.component.html',
+  styleUrls: ['./car-editor.component.scss'],
+})
+export class CarEditorComponent implements OnInit {
+  car$: Observable<Car> = this.ar.params.pipe(
+    switchMap((params) => this.carService.get(params['id']))
+  );
+
+  constructor(private ar: ActivatedRoute, private carService: CarService) {}
+
+  ngOnInit(): void {}
+
+  onUpdate(car: Car) {}
+}
