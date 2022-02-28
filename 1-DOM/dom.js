3077842256de5c8f12e1ef4e8d1d73223dcf8205
keszigabesz <91908624+keszigabesz@users.noreply.github.com>
2022-02-28 14:25:12 +0100

    1. kész

--- 1-DOM/dom.js
+++ 1-DOM/dom.js
@@ -1,5 +1,12 @@
 function manipulateDom() {
-    // 
+    const quotes = document.querySelectorAll('.quote');
+    quotes.forEach(item => {
+        item.style.borderColor = 'blue';
+        item.style.background = 'lightblue';
+        item.style.fontStyle = 'italic';
+    })
 };
 
-export { manipulateDom };
+export {
+    manipulateDom
+};
